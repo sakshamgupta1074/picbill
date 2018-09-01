058623e7d5553faebef3d23983d3e109c892bfd6
Sahil Batra <sahilbatra1211@gmail.com>
2018-09-02 02:26:12 +0530

    14th commit

--- routes/private.js
+++ routes/private.js
@@ -32,54 +32,46 @@ route.post('/vendor',(req,res)=>{
     })
 })
 
-route.post('/buycoupon',(req,res)=>{
-
+route.post('/buycoupon',(req,res)=> {
 
 
     coupon.findOne({
-        vendorname:req.body.vendorname
-    }).then((vendor)=>{
-        console.log(vendor);
-        user.findOne({
-            username:req.user.username
-        }).then((user)=>{
-            console.log(user);
-            if(user.noofcoupons<2){
-                user.noofcoupons=user.noofcoupons+1;
-                if(user.couponname1==='nocoupon')
-                {
-                    user.couponname1=vendor.vendorname
-                }
-                else{
-                    user.couponname2=vendor.vendorname
-                }
+        vendorname: req.body.vendorname
+    }).then((vendor) => {
+        if (req.user.cashback - parseInt(vendor.coupon1price) < 0) {
+            res.send(`Current amount:${req.user.cashback}<br/>sorry you dont have enough amount in your wallet`)
+        }
+        else {
+            user.findOne({
+                username: req.user.username
+            }).then((user) => {
+                console.log(user);
+
+                if (user.noofcoupons < 2) {
+                    user.noofcoupons = user.noofcoupons + 1;
+                    user.cashback = user.cashback - parseInt(vendor.coupon1price);
+
+
+                    if (user.couponname1 === 'nocoupon') {
+                        user.couponname1 = vendor.vendorname
+                    }
+                    else {
+                        user.couponname2 = vendor.vendorname
+                    }
                     user.save();
-                if(req.user.cashback-parseInt(vendor.coupon1price)<0){
-                    res.send(`Current amount:${req.user.cashback}<br/>sorry you dont have enough amount in your wallet`)
-                }
-                else {
-
-                    req.user.cashback = req.user.cashback - parseInt(vendor.coupon1price);
-
-                    req.user.save();
                     res.render('userprofile',{user:user})
 
-
+                }
+                else {
+                    res.send('sorry you cannot buy more than 2 coupons ')
                 }
 
-            }
-            else{
-                res.send('sorry you cannot buy more than 2 coupons ')
-            }
+
+            })
 
 
-
-
-        })
-
+        }
     })
-
-
 })
 
 route.get('/uploadbill',(req,res)=>{
@@ -104,14 +96,15 @@ route.post('/code',(req,res)=>{
         coupon.findOne({
            vendorpassword:req.body.password
         }).then((coupon)=>{
+            console.log(coupon);
             if(coupon.vendorname===req.user.couponname1){
-                couponname1='nocoupon'
+                req.user.couponname1='nocoupon'
             }
             else{
-                couponname2='nocoupon'
+                req.user.couponname2='nocoupon'
             }
             req.user.noofcoupons=req.user.noofcoupons-1;
-            user.save();
+            req.user.save();
         })
 
         res.send('code verified')
