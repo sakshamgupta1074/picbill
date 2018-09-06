20563fc761c51c752a7155941a9a60ff3f2c0943
Sahil Batra <sahilbatra1211@gmail.com>
2018-09-07 00:26:02 +0530

    keys added

---
+++ config/keys.js
@@ -0,0 +1,13 @@
+module.exports={
+    google:{
+        clientID:"353829824197-rgo2r1hht86h3e22pr4if5hr6epoimop.apps.googleusercontent.com",
+        clientSecret: "-PZeAjOR9kVfeBayREro58i0"
+
+    },
+    mongodb:{
+        dbURI: "mongodb://sahil:detective1@ds125322.mlab.com:25322/mycoupon"
+    },
+    session:{
+        cookiesKey:'youcannothack'
+    }
+}
